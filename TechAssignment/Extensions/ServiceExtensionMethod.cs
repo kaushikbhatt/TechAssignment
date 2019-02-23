2b605a102ba3f452572940e2300dc9dfd7ee6e67
ksbhatt487@hotmail.com <ksbhatt487@hotmail.com>
2019-02-24 04:26:05 +0700

    Startup file setup
    Setup with extension method and connectionstring

---
+++ TechAssignment/Extensions/ServiceExtensionMethod.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechnicalAssignment.Entities;
+
+namespace TechnicalAssignment.Extensions
+{
+    public static class ServiceExtensionMethod
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+        }
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration config)
+        {
+          
+            services.AddDbContext<RepositoryContext>(options =>
+ options.UseSqlServer(config.GetConnectionString("SqlDb")));
+
+        }
+
+
+      
+      
+    }
+}
--- TechAssignment/Startup.cs
+++ TechAssignment/Startup.cs
@@ -5,12 +5,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TechnicalAssignment.Entities;
+using TechnicalAssignment.Extensions;
 
-namespace TechAssignment
+namespace TechnicalAssignment
 {
     public class Startup
     {
@@ -24,6 +27,8 @@ namespace TechAssignment
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureCors();
+            services.ConfigureSqlContext(Configuration);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
