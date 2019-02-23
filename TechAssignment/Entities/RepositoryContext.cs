36a43f0502cb3c46c3d6d0c30522f1dc35956fd8
ksbhatt487@hotmail.com <ksbhatt487@hotmail.com>
2019-02-24 04:15:01 +0700

    Contex  created
    Contex Created

---
+++ TechAssignment/Entities/RepositoryContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechnicalAssignment.Entities.Model;
+
+namespace TechnicalAssignment.Entities
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+            : base(options)
+        {
+           
+        }
+
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Transactions> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=KAUSHIK\\SQLEXPRESS;Database=TechnicalAssignment;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
