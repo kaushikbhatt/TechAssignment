928b6137c7c5e8733f7b07c0a8e7357fd1539895
ksbhatt487@hotmail.com <ksbhatt487@hotmail.com>
2019-02-24 04:37:17 +0700

    genric Repo with BL
    genric repo and Created  CustomerInquiry  Logic

---
+++ TechAssignment/IRepository/ICustomerInquiry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using TechnicalAssignment.Entities.Model;
+using TechnicalAssignment.Entities.ViewDataModel;
+namespace TechnicalAssignment.IRepository
+{
+    public interface ICustomerInquiry : IBase<Customer>
+    {
+       CustomerInquiryViewModel GetCustomerTransaction(CustomerInquiryInputViewModel input);
+    }
+}
+
+
---
+++ TechAssignment/Repository/CustomerInquiry.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechnicalAssignment.Entities;
+using TechnicalAssignment.Entities.Model;
+using TechnicalAssignment.Entities.ViewDataModel;
+using TechnicalAssignment.IRepository;
+
+namespace TechnicalAssignment.Repository
+{
+    public class CustomerInquiry : Base<Customer>, ICustomerInquiry
+    {
+        public CustomerInquiry(RepositoryContext repositoryContext)
+         : base(repositoryContext)
+        {
+
+        }
+
+        public CustomerInquiryViewModel GetCustomerTransaction(CustomerInquiryInputViewModel input)
+        {
+
+            if(input.CustomerId == null)
+            {
+                return new CustomerInquiryViewModel(GetCustomerByEmail(input.Email))
+                {
+                    Transactions = RepositoryContext.Transactions
+                   .Where(a => a.Status == input.CustomerId).OrderByDescending(date => date.TransactionDatetime).Take(5)
+                };
+            }
+            return new CustomerInquiryViewModel(GetCustomerById(input.CustomerId))
+          {
+                Transactions = RepositoryContext.Transactions
+                    .Where(a => a.Status == input.CustomerId).OrderByDescending(date=>date.TransactionDatetime).Take(5)
+            };
+        }
+
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return FindAll()
+                .OrderBy(customer => customer.CustomerName);
+        }
+
+        public Customer GetCustomerById(string CustomerId)
+        {
+            return FindByCondition(Customer => Customer.CustomerId.Equals(CustomerId))
+                .DefaultIfEmpty(new Customer())
+                .FirstOrDefault();
+        }
+
+        public Customer GetCustomerByEmail(string Email)
+        {
+            return FindByCondition(Customer => Customer.ContactEmail.Equals(Email))
+                .DefaultIfEmpty(new Customer())
+                .FirstOrDefault();
+        }
+
+
+        public void CreateCustomer(Customer Customer)
+        {
+        
+            Create(Customer);
+            Save();
+        }
+
+        public void UpdateCustomer(Customer Customer)
+        {
+         
+            Update(Customer);
+            Save();
+        }
+
+        public void DeleteCustomer(Customer Customer)
+        {
+            Delete(Customer);
+            Save();
+        }
+    }
+}
